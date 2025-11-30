2b6306b01213a3d5019204df17d43f50bb056176
Saisimir Vempali <simir2k5@gmail.com>
2025-11-30 04:53:57 -0500

    removed JWT based auth and shifted the authentication to clerk instead

--- roomie-backend/controllers/auth.controller.js
+++ roomie-backend/controllers/auth.controller.js
@@ -1,49 +1,37 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-
 import User from '../models/user.model.js';
-import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/env.js';
 
 export const signUp = async(req, res, next) => {
-  const session = await mongoose.startSession();
-  session.startTransaction();
-
   try {
-    const { name, email, password } =req.body;
+    const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({email});
+    let user = await User.findOne({email});
 
-    if(existingUser) {
+    if(user) {
         const error = new Error('User already exists');
         error.statusCode = 409;
         throw error;
     }
 
-    //Hash password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-    const newUsers = await User.create([{name, email, password: hashedPassword }], {session})
-
-    const token = jwt.sign({userID: newUsers[0]._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN})
-
-    await session.commitTransaction();
-    session.endSession();
+    user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    
 
     res.status(201).json({
-       success: true,
-       message: 'User created successfully',
-       data: {
-        token,
-        user: newUsers[0],
-       }
-    })
-  } catch (error) {
-    await session.abortTransaction();
-    session.endSession();
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch(error){
     next(error);
-    }
+  }
 }
 
 export const logIn = async(req, res, next) => {
@@ -65,8 +53,6 @@ export const logIn = async(req, res, next) => {
         error.statusCode = 401;
         throw error;
     }
-
-    const token = jwt.sign({userId: user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
 
     res.status(200).json({
         success: true,
